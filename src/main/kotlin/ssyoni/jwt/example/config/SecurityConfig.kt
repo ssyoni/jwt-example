d5eee43a9834cd96712096a42baff53d888a8407
ssyoni <jsy950601@gmail.com>
2022-11-06 16:16:00 +0900

    401 UnAuthoried 해결을 위한 security 설정

---
+++ src/main/kotlin/ssyoni/jwt/example/config/SecurityConfig.kt
@@ -0,0 +1,21 @@
+package ssyoni.jwt.example.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration
+import org.springframework.security.web.SecurityFilterChain
+
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain{
+        http
+            .authorizeRequests()    // httpServletRequest를 사용하는 요청에 대해 접근 제한을 설정
+            .antMatchers("/api/hello").permitAll() // 해당 api에 대해선 인증 없이 접근 허용
+            .anyRequest().authenticated()   //  나머지 접근들에 대해서는 인증을 받아야함
+
+        return http.build()
+    }
+}

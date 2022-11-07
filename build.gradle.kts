a26887fbc1e7a7e007d3d88afeb5001e16d52532
ssyoni <jsy950601@gmail.com>
2022-11-07 22:28:29 +0900

    chore: jwt 관련 설정
    - modify application.yml
    - modify build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,11 @@ dependencies {
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
+
+	//jwt
+	implementation("io.jsonwebtoken:jjwt-api:0.11.2")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
 }
 
 tasks.withType<KotlinCompile> {

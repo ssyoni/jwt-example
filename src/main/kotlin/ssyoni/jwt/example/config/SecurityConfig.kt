ce138d0895afe6cb3625f3638e52df02a231f2ab
ssyoni <jsy950601@gmail.com>
2022-11-07 22:17:43 +0900

    style: H2 db 관련 설정
    - modify SecurityConfig
    - create data.sql

--- src/main/kotlin/ssyoni/jwt/example/config/SecurityConfig.kt
+++ src/main/kotlin/ssyoni/jwt/example/config/SecurityConfig.kt
@@ -2,12 +2,18 @@ package ssyoni.jwt.example.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.web.SecurityFilterChain
 
 @EnableWebSecurity
 class SecurityConfig {
+
+    @Bean
+    fun webSecurityCustomizer():WebSecurityCustomizer{
+        return WebSecurityCustomizer { web:WebSecurity -> web.ignoring().antMatchers("/h2-console/**", "/favicon.ico") }
+    }
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain{
